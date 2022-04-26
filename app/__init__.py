8dfbaf456e2463462218e926cd3f2978016dea18
Daniel Jr <daniel.souza.lima@outlook.com>
2022-04-25 23:54:31 -0300

    using flask restfull

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
+from flask_restful import Api
 
 db_api = Flask(__name__)
-
+api = Api(db_api)
 Bcrypt(db_api) # see it later...
 
 from app.routes import *
