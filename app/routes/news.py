6c9b14f784deb12b4a649aaa5f52970464963224
Daniel Jr <daniel.souza.lima@outlook.com>
2022-04-25 23:55:00 -0300

    using flask RESTful in news routes

--- app/routes/news.py
+++ app/routes/news.py
@@ -1,64 +1,64 @@
 from app.controllers.news_controller import NewsController 
-from app import db_api
 from flask import request
+from flask_restful import Resource
+from app import api
 
 NEWS = NewsController()
 
-@db_api.route('/news/', methods=['POST'])
-def create_news():
-    data = request.get_json()
-    NEWS.create(data['title'], data['image'], data['content'])
+class NewsAPI(Resource):
 
-    return 'News created', 201
+    def post(self):
+        data = request.get_json()
+        NEWS.create(data['title'], data['image'], data['content'])
+
+        return 'News created', 201
 
 
-@db_api.route('/news/', methods=['PUT'])
-def update_news():
-    data = request.get_json()
-    NEWS.update(data['id'], data['title'], data['image'], data['content'])
+    def put(self, id):
+        id = str(id)
+        data = request.get_json()
+        NEWS.update(id, data['title'], data['image'], data['content'])
+        
+        return 'News updated', 200
+
+
+    def delete(self, id):
+        id = str(id)
+        NEWS.delete(id)
     
-    return 'News updated', 200
+        return 'News Deleted', 200
 
 
-@db_api.route('/news/', methods=['DELETE'])
-def delete_news():
-    data = request.get_json()
-    NEWS.delete(data['id'])
-  
-    return 'News Deleted', 200
+    def get(self, id=None):
+        if not id:
+            list_of_news = NEWS.list()
+            response = []
 
+            for news in list_of_news:
+                response.append({
+                    "id": news.id,
+                    "title": news.title,
+                    "image": news.image,
+                    "content": news.content 
+                })
+            
+            response = {
+                "news": response,
+                "length": len(response)
+            }
 
-@db_api.route('/news/<uuid:id>', methods=['GET'])
-def retrieve_news():
-    id = request.args.get('id', type = str)
-    retrieved_news = NEWS.retrieve(id)
+            return response, 200
 
-    response = {
-        "id": retrieved_news.id,
-        "title": retrieved_news.title,
-        "image": retrieved_news.image,
-        "content": retrieved_news.content 
-    }
-    
-    return response, 200
+        id = str(id)
+        retrieved_news = NEWS.retrieve(id)
 
+        response = {
+            "id": retrieved_news.id,
+            "title": retrieved_news.title,
+            "image": retrieved_news.image,
+            "content": retrieved_news.content 
+        }
+        
+        return response, 200
 
-@db_api.route('/news/')
-def list_news():
-    list_of_news = NEWS.list()
-    response = []
-
-    for news in list_of_news:
-        response.append({
-            "id": news.id,
-            "title": news.title,
-            "image": news.image,
-            "content": news.content 
-        })
-    
-    response = {
-        "news": response,
-        "length": len(response)
-    }
-
-    return response, 200
+api.add_resource(NewsAPI, '/news/', '/news/<uuid:id>')
