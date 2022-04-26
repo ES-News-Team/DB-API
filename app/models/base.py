5b0abd21a87e5f4c1ac2f360fa6da1748d08acb9
Daniel Jr <daniel.souza.lima@outlook.com>
2022-04-25 22:34:05 -0300

    definging metadata in ENGINE

--- app/models/base.py
+++ app/models/base.py
@@ -1,8 +1,17 @@
 from sqlalchemy.ext.declarative import declarative_base
 import sqlalchemy
 
+DATABASE_INFO = {
+    "user": "root",
+    "password": "1234567890",
+    "host": "mariadb",
+    "port": "3306" 
+} 
+
 # Define the MariaDB engine using MariaDB Connector/Python
-ENGINE = sqlalchemy.create_engine("mariadb+mariadbconnector://root:1234567890@mariadb:3306/esnews")
+ENGINE = sqlalchemy.create_engine(
+    f"mariadb+mariadbconnector://{DATABASE_INFO['user']}:{DATABASE_INFO['password']}@{DATABASE_INFO['host']}:{DATABASE_INFO['port']}/esnews"
+)
 
 BASE = declarative_base()
 
